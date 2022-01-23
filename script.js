c15dd3311638a40f83f3b39004a1d43a74b01bd8
muneer-hussain <97391190+muneer-hussain@users.noreply.github.com>
2022-01-23 18:22:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// clock 
+
+// for adding zero in number less than 10 
+function twoDigits(n) {
+    if (n < 10)
+        return '0' + n;
+    return n;
+}
+
+// main function for clock
+function clock() {
+    var d = new Date;
+
+    let hours = d.getHours();
+    let minutes = d.getMinutes();
+    let seconds = d.getSeconds();
+    let session = " ";
+
+    hours = hours % 12
+
+    if (hours < 12) {
+        session = "PM";
+    } else {
+        session = "AM"
+    }
+    document.getElementById("fulldate").innerHTML = `${twoDigits (hours)}  : ${twoDigits (minutes)} : ${twoDigits (seconds)} ${session}`;
+
+    setTimeout(clock, 1000);
+}
+
+clock();
