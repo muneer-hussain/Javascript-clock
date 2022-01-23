34dfd34d566fc9b54082131f351e128e1e255848
muneer-hussain <97391190+muneer-hussain@users.noreply.github.com>
2022-01-24 03:19:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
 // clock 
-
-// for adding zero in number less than 10 
-function twoDigits(n) {
+    
+    // for adding zero in number less than 10 
+    function twoDigits(n) {
     if (n < 10)
-        return '0' + n;
+    return '0' + n;
     return n;
-}
-
-// main function for clock
-function clock() {
+    }
+    
+    // main function for clock
+    function clock() {
     var d = new Date;
-
+    
     let hours = d.getHours();
     let minutes = d.getMinutes();
     let seconds = d.getSeconds();
-    let session = " ";
+    var session = "AM";
+    
 
-    hours = hours % 12
-
-    if (hours < 12) {
-        session = "PM";
-    } else {
-        session = "AM"
+    if(hours == 0){
+        hours = 12;
     }
+    
+    if(hours > 12){
+        hours = hours - 12;
+        session = "PM";
+    };
+    
+	hours % 12;
+	
     document.getElementById("fulldate").innerHTML = `${twoDigits (hours)}  : ${twoDigits (minutes)} : ${twoDigits (seconds)} ${session}`;
-
+    
     setTimeout(clock, 1000);
-}
-
-clock();
+    }
+    
+    clock();
